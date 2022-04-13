d67a0eeb11c125f3d5180dd327d12337246225a0
voXrey <voxrey@gmail.com>
2022-04-13 19:24:20 +0200

    Générateur de tableau

---
+++ core/functions.py
@@ -0,0 +1,15 @@
+
+
+def creer_tableau(lignes:int, colonnes:int) -> list[list[int]]:
+    """
+    Créer un tableau rempli de 0
+
+    Args:
+        lignes (int): nombre de lignes
+        colonnes (int): nombre de colonnes
+
+    Returns:
+        list[list[int]]: liste de listes de 0
+    """
+    return [[0 for i in range(colonnes)] for j in range(lignes)]
+
