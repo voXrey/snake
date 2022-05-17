2635007414bf738cfd78fe2d2d5925774fe7b775
voXrey <72698969+voXrey@users.noreply.github.com>
2022-05-17 17:05:56 +0200

    Création de nouvelle_position
    Fonction qui calculera la prochaine positon de la tête

--- core/functions.py
+++ core/functions.py
@@ -1,4 +1,5 @@
 from tkinter import IntVar
+from classes import Snake
 
 def creer_tableau(lignes:int, colonnes:int) -> list[list[int]]:
     """
@@ -24,3 +25,28 @@ def update_score(score_var:IntVar, score:int) -> None:
     actuel = score_var.get()
     score_var.set(actuel+score)
 
+def nouvelle_position(prochain_mouvement:str, serpent:Snake, tab:list[list[int]]) -> tuple[int]:
+    """
+    Calcule la prochaine position de la tête sur le tableau
+
+    Args:
+        prochain_mouvement (str): direction vers laquelle part le serpent
+        serpent (Snake): le serpent
+        tab (list[list[int]]): tableau du jeu
+
+    Returns:
+        tuple[int]: prochaine position de la têtes
+    """
+    # On récupère la position actuelle de la tête du serpent
+    position_tete = serpent.corps[0].position
+
+    # On teste la variable prochain_mouvement pour connaître
+    # la direction du serpent
+    if prochain_mouvement == "haut":
+        pass
+    elif prochain_mouvement == "bas":
+        pass
+    elif prochain_mouvement == "gauche":
+        pass
+    elif prochain_mouvement == "droite":
+        pass
