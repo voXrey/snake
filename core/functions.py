29a42cbfca7ab3ac27411de897db625a992f48fc
voXrey <72698969+voXrey@users.noreply.github.com>
2022-05-17 16:48:32 +0200

    Ajout de update_score
    Cette fonction permet de mettre à jour le score

--- core/functions.py
+++ core/functions.py
@@ -1,4 +1,4 @@
-
+from tkinter import IntVar
 
 def creer_tableau(lignes:int, colonnes:int) -> list[list[int]]:
     """
@@ -12,3 +12,15 @@ def creer_tableau(lignes:int, colonnes:int) -> list[list[int]]:
         list[list[int]]: liste de listes de 0
     """
     return [[0 for i in range(colonnes)] for j in range(lignes)]
+
+def update_score(score_var:IntVar, score:int) -> None:
+    """
+    Met à jour le score
+
+    Args:
+        score_var (IntVar): variable tkinter qui représente le score
+        score (int): score à ajouter
+    """
+    actuel = score_var.get()
+    score_var.set(actuel+score)
+
